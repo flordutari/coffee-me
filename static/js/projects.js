ffe957d9fed3c45721e40ed48abcbbcce1ed91b3
Flor <fdutari2@gmail.com>
2021-02-06 12:16:19 +0100

    add function to modify button text

--- static/js/projects.js
+++ static/js/projects.js
@@ -1,6 +1,8 @@
 const cardProjectDescription = document.querySelectorAll('.COF-card-project-description');
 const coffeeValueButtons = document.querySelectorAll('.COF-primary-box');
 const coffeesQuantityInput = document.querySelector('#coffeesQuantity');
+const buttonBuyCoffee = document.querySelector('#buyCoffee');
+const inputCoffeesQuantity = document.querySelector('#coffeesQuantity');
 
 [...cardProjectDescription].forEach(card => {
     const reducedText = card && `${card.textContent.substr(0, 120)}...`;
@@ -10,6 +12,23 @@ const coffeesQuantityInput = document.querySelector('#coffeesQuantity');
 coffeeValueButtons.forEach(button => {
     button.addEventListener('click', e => {
         e.preventDefault();
-        coffeesQuantityInput.value = e.target.textContent;
+        const quantity = e.target.textContent;
+        coffeesQuantityInput.value = quantity;
+        if (e.target.textContent > 1) {
+            buttonBuyCoffee.innerHTML = `Buy ${quantity} coffees (USD $${quantity * 3})`;
+        } else {
+            buttonBuyCoffee.innerHTML = 'Buy a coffee (USD $3)';
+        }
     });
 });
+
+if (inputCoffeesQuantity) {
+    inputCoffeesQuantity.addEventListener('keyup', e => {
+        const quantity = e.target.value;
+        if (quantity > 1) {
+            buttonBuyCoffee.innerHTML = `Buy ${quantity} coffees (USD $${quantity * 3})`;
+        } else {
+            buttonBuyCoffee.innerHTML = 'Buy a coffee (USD $3)';
+        }
+    })
+}
